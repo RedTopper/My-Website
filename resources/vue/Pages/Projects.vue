ef95841fe556a2d59554af4891db52e421ed4504
AJ Walter <git@awalter.net>
2021-04-12 22:53:29 -0500

    Add projects

---
+++ resources/vue/Pages/Projects.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="content">
+		<div v-for="(app, index) in apps" :key="index" v-on:click="cmdLaunch(app)">
+			<div class="app">
+				<img class="icon" :src="app.icon" :alt="app.label"/>
+				<div class="title">{{ app.title }}</div>
+				<div class="description">{{ app.label }}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import {Component, Emit, Prop, Vue} from "vue-property-decorator";
+
+import {IApp} from "@ts/App/IApp";
+
+@Component
+export default class Projects extends Vue {
+	@Prop() apps!: IApp[];
+
+	@Emit()
+	cmdLaunch(app: IApp) {
+		return app;
+	}
+}
+</script>
+
+<style lang="sass" scoped>
+.content
+	display: flex
+	flex-wrap: wrap
+	height: 100%
+	padding: 8px
+	box-sizing: border-box
+
+.app
+	color: white
+	margin: 8px
+	text-align: center
+
+.app .icon
+	width: 64px
+	stroke: white
+	fill: white !important
+	display: block
+	margin: 0 auto
+
+.app .title
+	margin-top: 4px
+
+</style>
